a44c968c17c9421f59e265eef1f195e9aa151d8c
not-inept <mason@notinept.me>
2017-10-27 17:47:13 -0400

    In a surprising turn of events (hehe), I figured out how to inject without breaking the service.

--- sticky.js
+++ sticky.js
@@ -1,3 +1,47 @@
 (function(window, document) {
+  function insert_table_header() {
+    var label_th = document.createElement('th');
+    label_th.innerHTML = "Label";
+    label_th.className += "mdl-data-table__cell--non-numeric";
+
+    var tags_th = document.createElement('th');
+    tags_th.innerHTML = "Tags";
+    tags_th.className += " mdl-data-table__cell--non-numeric";
+    tags_th.className += "all-clicks";
+
+    var thead = document.querySelector("body > div.main > div.shorten.content.constrain > table > thead > tr");
+    thead.appendChild(label_th);
+    thead.appendChild(tags_th);
+  }
+
+  function insert_data(element) {
+    var short_url = element.querySelector(".short-url > a").textContent;
+    var id = short_url.split('/')[1];
+
+    var label = document.createElement("td");
+    label.innerHTML = id;
+    label.className += " mdl-data-table__cell--non-numeric";
+    element.appendChild(label);
+
+    var tags = document.createElement("td");
+    tags.innerHTML = "tags, tags";
+    tags.className += " mdl-data-table__cell--non-numeric";
+    element.appendChild(tags);
+  }
+
   console.log("Working on it :)");
+  window.onload = function() {
+    console.log("Loaded :)");
+    insert_table_header();
+    var tbody = document.querySelector("body > div.main > div.shorten.content.constrain > table > tbody");
+    // The mutation observer is an amazing thing-- look it up, it's magic!
+    var observer = new MutationObserver(function(mutations) {
+      mutations.forEach(function(mutation) {
+        mutation.addedNodes.forEach(insert_data);
+      });
+    });
+    var config = { childList: true };
+    observer.observe(tbody, config);
+  };
+
 }(window, document));
